aba8e0ae361b32fb997afe0b292ffe3303275b3c
violetskies11757 <shinyfairyeevee9922@outlook.com>
2024-11-27 09:38:29 -0800

    Enemies spawn in waves; All screens have a background

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,6 +9,9 @@ public class SpawnManager : MonoBehaviour
     public float verticalRange;
     public GameObject player;
 
+    public GameObject enemyProjectile;
+    public int enemyProjectilesToSpawn;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,14 +24,33 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(GameObject.FindGameObjectsWithTag("EnemyProjectile").Length <= 0)
+        {
+            SpawnWave(enemyProjectilesToSpawn);
+        }
     }
 
     public Vector3 GenerateRandomPosition()
     {
         float randomX = Random.Range(-horizontalRange, horizontalRange);
         float randomY = Random.Range(-verticalRange, verticalRange);
+        Vector3 randomPos = new Vector3(randomX, randomY, 0);
 
-        return new Vector3(randomX, randomY, 0);
+        while((randomPos - player.transform.position).magnitude < gm.safetyRadius)
+        {
+             randomX = Random.Range(-horizontalRange, horizontalRange);
+             randomY = Random.Range(-verticalRange, verticalRange);
+             randomPos = new Vector3(randomX, randomY, 0);
+        }
+
+        return randomPos;
+    }
+
+    public void SpawnWave(int enemyProjectiles)
+    {
+        for(int i = 0; i < enemyProjectiles; i++)
+        {
+            Instantiate(enemyProjectile, GenerateRandomPosition(), transform.rotation);
+        }
     }
 }
